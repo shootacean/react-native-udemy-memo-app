d70ddc6279da967193e5e0476c0b3706d51c4627
Shota Narisaka <shootacean@gmail.com>
2024-06-10 06:38:47 +0900

    ci: change biome rules(all=true)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+// biome-ignore lint: lint/style/noDefaultExport
 export default function App() {
 	return (
 		<View style={styles.container}>
