99663d7632fb811ff8ac187237f13a97b5f1613c
Shota Narisaka <shootacean@gmail.com>
2024-06-11 06:42:47 +0900

    feat: use expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import { Slot } from "expo-router";
+
+const Layout = () => {
+	return (
+		<>
+			<Text>Layout</Text>
+			<Slot />
+		</>
+	);
+};
+
+export default Layout;
---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import "expo-router/entry";
+import { Text, View } from "react-native";
+
+export const Index = () => {
+	return (
+		<View>
+			<Text>Hello, world!</Text>
+		</View>
+	);
+};
+
+export default Index;
