cf3d790cc9f5a2e8e06c21744c65f18dd82616a5
Shota Narisaka <shootacean@gmail.com>
2024-06-11 06:51:39 +0900

    feat: マークアップの仮実装

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,43 @@
 import "expo-router/entry";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export const Index = () => {
 	return (
-		<View>
-			<Text>Hello, world!</Text>
+		<View style={styles.container}>
+			{/* Header */}
+			<View>
+				<Text>Memo App</Text>
+				<Text>ログアウト</Text>
+			</View>
+			{/* List */}
+			<View>
+				{/* List Item */}
+				<View>
+					<Text>買い物リスト</Text>
+					<Text>2020年12月24日 10:00</Text>
+					<View>
+						<Text>X</Text>
+					</View>
+				</View>
+				<View>
+					<Text>買い物リスト</Text>
+					<Text>2020年12月24日 10:00</Text>
+					<View>
+						<Text>X</Text>
+					</View>
+				</View>
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	}
+});
 
 export default Index;
