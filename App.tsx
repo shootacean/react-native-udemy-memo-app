00000e1f6a33d71b90596acdf7ebf6ea6ade0a92
Shota Narisaka <shootacean@gmail.com>
2024-06-11 06:10:55 +0900

    feat: セクション10 はじめてのコンポーネント

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Hello } from "./src/components/Hello";
 
 // biome-ignore lint: lint/style/noDefaultExport
 export default function App() {
 	return (
 		<View style={styles.container}>
+			<Hello style={{fontSize: 16}}>World</Hello>
 			<Text>Open up App.tsx to start working on your app!</Text>
 			<StatusBar style="auto" />
 		</View>
---
+++ src/components/Hello.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from "react";
+import { StyleSheet, Text, View, type TextStyle } from "react-native";
+
+interface HelloProps {
+	children: ReactNode;
+	bang?: boolean;
+	style?: TextStyle;
+}
+
+export const Hello = ({children, bang, style}: HelloProps) => {
+	return (
+		<View>
+			<Text style={[styles.text, style]}>
+				Hello {children} {bang ? "!" : ""}
+			</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	text: {
+		padding: 16,
+		backgroundColor: "blue",
+		fontSize: 40,
+		fontWeight: "bold",
+		color: "white",
+	},
+});
