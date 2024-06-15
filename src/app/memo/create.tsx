0f67c09e3acb363c89472a1752832577db8625d9
Shota Narisaka <shootacean@gmail.com>
2024-06-16 06:39:04 +0900

    feat: メモ作成をFirestoreに保存する

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,20 +1,50 @@
 import { router } from "expo-router";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
 import {
 	KeyboardAvoidingView,
 	StyleSheet,
 	TextInput,
 	View,
 } from "react-native";
+import { auth, db } from "../../config";
+
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 
+async function handleCreate(bodyText: string) {
+	if (!auth.currentUser) {
+		// ログインしていない場合は何もせずログイン画面へ遷移する
+		router.replace("/auth/login");
+		return;
+	}
+	const ref = collection(db, `users/${auth.currentUser?.uid}/memos`);
+	try {
+		await addDoc(ref, {
+			bodyText,
+			cretatedAt: Timestamp.fromDate(new Date()),
+			updatedAt: Timestamp.fromDate(new Date()),
+		});
+		router.back();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
 const Create = () => {
+	const [bodyText, setBodyText] = useState("");
+
 	return (
 		<KeyboardAvoidingView behavior="height" style={styles.container}>
 			<View style={styles.inputContainer}>
-				<TextInput style={styles.input} multiline={true} />
+				<TextInput
+					style={styles.input}
+					multiline={true}
+					value={bodyText}
+					onChangeText={(text) => setBodyText(text)}
+				/>
 			</View>
-			<CircleButton onPress={() => router.back()}>
+			<CircleButton onPress={() => handleCreate(bodyText)}>
 				<Icon name="check" size={40} color="#ffffff" />
 			</CircleButton>
 		</KeyboardAvoidingView>
