6e3a93b3d7e1eeeb3aa131bcb2164a3301d6abc3
Shota Narisaka <shootacean@gmail.com>
2024-06-16 06:15:31 +0900

    feat: ログイン済みの場合はメモ一覧画面を表示する

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,19 @@
-import { Redirect } from "expo-router";
+import { Redirect, router } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+
+import { auth } from "../config";
 
 const Index = () => {
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// ログイン済みであればメモ一覧画面へ遷移
+				router.replace("/memo/list");
+			}
+		});
+	}, []);
+
 	return <Redirect href="/auth/signup" />;
 	// return <Redirect href="/auth/login" />;
 	// return <Redirect href="/memo/list" />;
