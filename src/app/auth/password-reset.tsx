f3223e1a39ca0d2b64aef839c38d46ae4db73f8b
Shota Narisaka <shootacean@gmail.com>
2024-06-29 07:34:34 +0900

    doc: add link github issue

--- src/app/auth/password-reset.tsx
+++ src/app/auth/password-reset.tsx
@@ -11,6 +11,7 @@ export const PasswordReset = () => {
 	const handlePasswordReset = async (email: string) => {
 		try {
 			// MEMO: Firebaseの「メールの列挙保護」を有効にしているため、メールアドレスが存在しない場合も「パスワードリセットメールを送信しました」と表示される
+			// https://github.com/firebase/firebase-js-sdk/issues/7651
 			await sendPasswordResetEmail(auth, email);
 			Alert.alert(
 				"パスワードリセットメールを送信しました。\nメールをご確認ください。\n（メールが届かない場合は、入力したメールアドレスを見直す、または迷惑メールフォルダをご確認ください）",
