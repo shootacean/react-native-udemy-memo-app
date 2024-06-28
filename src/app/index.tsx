079c0b6f51f38634c9422ad9656de22a51ecb664
Shota Narisaka <shootacean@gmail.com>
2024-06-28 23:26:16 +0900

    refactor: remove comment

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -14,12 +14,7 @@ const Index = () => {
 		});
 	}, []);
 
-	return <Redirect href="/auth/signup" />;
-	// return <Redirect href="/auth/login" />;
-	// return <Redirect href="/memo/list" />;
-	// return <Redirect href="/memo/detail" />;
-	// return <Redirect href="/memo/edit" />;
-	// return <Redirect href="/memo/create" />;
+	return <Redirect href="/auth/login" />;
 };
 
 export default Index;
